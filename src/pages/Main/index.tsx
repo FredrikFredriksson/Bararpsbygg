a64d934e9bac6d009bf30630dbbea7efe47f3634
Fredrik <fredrik.fredriksson@coin360.com>
2025-02-16 13:50:52 +0100

    fix

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import "./styles.scss";
 import Header from "../../components/Header";
 import Greetings from "../../components/Greeting";
